ce4e48c9827f00e90937977eb3bd7df38638e24a
Cristine <christinekhong8@gmail.com>
2021-11-04 15:10:42 +0700

    sensor aggregation 90 done

--- SalaryConversion/app/Controllers/Sensor.php
+++ SalaryConversion/app/Controllers/Sensor.php
@@ -65,9 +65,147 @@ class Sensor extends BaseController
                 // $this->sensorModel->save($arrInsert);
             }
         }
-        dd($data_room1, $data_room2, $data_room3);
+
+        // data min, max, med, av for room1
+        $min_room1_temp = min(array_column($data_room1, 'temperature'));
+        $max_room1_temp = max(array_column($data_room1, 'temperature'));;
+        $av_room1_temp = 0;
+        $med_room1_temp = 0;
+
+        // data min, max, med, av for room2
+        $min_room2_temp = min(array_column($data_room2, 'temperature'));;
+        $max_room2_temp = max(array_column($data_room2, 'temperature'));;
+        $av_room2_temp = 0;
+        $med_room2_temp = 0;
+
+        // data min, max, med, av for room3
+        $min_room3_temp = min(array_column($data_room3, 'temperature'));;
+        $max_room3_temp = max(array_column($data_room3, 'temperature'));;
+        $av_room3_temp = 0;
+        $med_room3_temp = 0;
+
+        // data min, max, med, av for room1
+        $min_room1_hum = min(array_column($data_room1, 'humidity'));
+        $max_room1_hum = max(array_column($data_room1, 'humidity'));;
+        $av_room1_hum = 0;
+        $med_room1_hum = 0;
+
+        // data min, max, med, av for room2
+        $min_room2_hum = min(array_column($data_room2, 'humidity'));;
+        $max_room2_hum = max(array_column($data_room2, 'humidity'));;
+        $av_room2_hum = 0;
+        $med_room2_hum = 0;
+
+        // data min, max, med, av for room3
+        $min_room3_hum = min(array_column($data_room3, 'humidity'));;
+        $max_room3_hum = max(array_column($data_room3, 'humidity'));;
+        $av_room3_hum = 0;
+        $med_room3_hum = 0;
+
+        if ($data_room1) {
+            $count = count($data_room1);
+            $mid = floor(($count - 1) / 2);
+            if ($count % 2) {
+                $med_room1_temp = $data_room1[$mid]['temperature'];
+                $med_room1_hum = $data_room1[$mid]['humidity'];
+            } else {
+                $low_1 = $data_room1[$mid]['temperature'];
+                $high_1 = $data_room1[$mid + 1]['temperature'];
+                $med_room1_temp = (($low_1 + $high_1) / 2);
+
+                $low_2 = $data_room1[$mid]['humidity'];
+                $high_2 = $data_room1[$mid + 1]['humidity'];
+                $med_room1_hum = (($low_2 + $high_2) / 2);
+            }
+
+            $total_hum = 0;
+            $total_temp = 0;
+            foreach ($data_room1 as $room1) {
+                $total_temp = $total_temp + $room1['temperature'];
+                $total_hum = $total_hum + $room1['humidity'];
+            }
+            $av_room1_temp = ($total_temp / $count);
+            $av_room1_hum = ($total_hum / $count);
+        }
+
+        if ($data_room2) {
+            $count = count($data_room2);
+            $mid = floor(($count - 1) / 2);
+            if ($count % 2) {
+                $med_room2_temp = $data_room2[$mid]['temperature'];
+                $med_room2_hum = $data_room2[$mid]['humidity'];
+            } else {
+                $low_1 = $data_room2[$mid]['temperature'];
+                $high_1 = $data_room2[$mid + 1]['temperature'];
+                $med_room2_temp = (($low_1 + $high_1) / 2);
+
+                $low_2 = $data_room2[$mid]['humidity'];
+                $high_2 = $data_room2[$mid + 1]['humidity'];
+                $med_room2_hum = (($low_2 + $high_2) / 2);
+            }
+
+            $total_hum = 0;
+            $total_temp = 0;
+            foreach ($data_room2 as $room2) {
+                $total_temp = $total_temp + $room2['temperature'];
+                $total_hum = $total_hum + $room2['humidity'];
+            }
+            $av_room2_temp = ($total_temp / $count);
+            $av_room2_hum = ($total_hum / $count);
+        }
+
+        if ($data_room3) {
+            $count = count($data_room3);
+            $mid = floor(($count - 1) / 2);
+            if ($count % 2) {
+                $med_room3_temp = $data_room3[$mid]['temperature'];
+                $med_room3_hum = $data_room3[$mid]['humidity'];
+            } else {
+                $low_1 = $data_room3[$mid]['temperature'];
+                $high_1 = $data_room3[$mid + 1]['temperature'];
+                $med_room3_temp = (($low_1 + $high_1) / 2);
+
+                $low_2 = $data_room3[$mid]['humidity'];
+                $high_2 = $data_room3[$mid + 1]['humidity'];
+                $med_room3_hum = (($low_2 + $high_2) / 2);
+            }
+
+            $total_hum = 0;
+            $total_temp = 0;
+            foreach ($data_room3 as $room1) {
+                $total_temp = $total_temp + $room1['temperature'];
+                $total_hum = $total_hum + $room1['humidity'];
+            }
+            $av_room3_temp = ($total_temp / $count);
+            $av_room3_hum = ($total_hum / $count);
+        }
+
+        $room1_temp = [];
+        $room2_temp = [];
+        $room3_temp = [];
+        array_push($room1_temp, $min_room1_temp, $max_room1_temp, $med_room1_temp, $av_room1_temp);
+        array_push($room2_temp, $min_room2_temp, $max_room2_temp, $med_room2_temp, $av_room2_temp);
+        array_push($room3_temp, $min_room3_temp, $max_room3_temp, $med_room3_temp, $av_room3_temp);
+
+        $room1_hum = [];
+        $room2_hum = [];
+        $room3_hum = [];
+        array_push($room1_hum, $min_room1_hum, $max_room1_hum, $med_room1_hum, $av_room1_hum);
+        array_push($room2_hum, $min_room2_hum, $max_room2_hum, $med_room2_hum, $av_room2_hum);
+        array_push($room3_hum, $min_room3_hum, $max_room3_hum, $med_room3_hum, $av_room3_hum);
+
+
         $data = [
             'title' => 'Sensor Aggregation',
+            'room1_temp' => $room1_temp,
+            'room2_temp' => $room2_temp,
+            'room3_temp' => $room3_temp,
+            'room1_hum' => $room1_hum,
+            'room2_hum' => $room2_hum,
+            'room3_hum' => $room3_hum,
+            'data_room1' => $data_room1,
+            'data_room2' => $data_room2,
+            'data_room3' => $data_room3
         ];
         return view('pages/sensor_agre', $data);
     }
--- SalaryConversion/app/Views/pages/sensor_agre.php
+++ SalaryConversion/app/Views/pages/sensor_agre.php
@@ -6,39 +6,80 @@
     <div class="row">
         <div class="col">
             <h1>Sensor Aggregation</h1>
-
-            <div class="container-white">
-                <div class="row-g">
-                    <div class="col-g">
-                        <h2 class="title-stat">Temperatur</h2>
-                    </div>
-
-                    <div class="col-g">
-                        <h2 class="title-stat">Humidity</h2>
-                    </div>
-                </div>
-                <div class="row-g p-5">
-                    <div class="col-g">
-                        <div class="container-white">
-                            <h2>Temperatur</h2>
-                            <canvas id="temperatur" width="400" height="400"></canvas>
+            <?php for ($i = 0; $i < 3; $i++) { ?>
+                <?php
+                $data_temp = [];
+                $id_temp = "";
+                if ($i == 0) {
+                    $data_temp = $room1_temp;
+                    $id_temp = "temperatur1";
+                } else if ($i == 2) {
+                    $data_temp = $room2_temp;
+                    $id_temp = "temperatur2";
+                } else {
+                    $data_temp = $room3_temp;
+                    $id_temp = "temperatur3";
+                }
+                $data_hum = [];
+                $id_hum = "";
+                if ($i == 0) {
+                    $data_hum = $room1_hum;
+                    $id_hum = "humidity1";
+                } else if ($i == 2) {
+                    $data_hum = $room2_hum;
+                    $id_hum = "humidity2";
+                } else {
+                    $data_hum = $room3_hum;
+                    $id_hum = "humidity3";
+                }
+                ?>
+                <div class="container-white">
+                    <div class="row-g">
+                        <div class="col-g">
+                            <h2 class="title-stat">Room-<?= $i + 1; ?></h2>
                         </div>
                     </div>
-                    <div class="col-g">
-                        <div class="container-white">
-                            <h2>Humidity</h2>
-                            <canvas id="humidity" width="400" height="400"></canvas>
+                    <div class="row-g p-5">
+                        <div class="col-g">
+                            <div class="container-white">
+                                <h2>Temperatur</h2>
+                                <canvas id="<?= $id_temp; ?>" width="400" height="400"></canvas>
+                            </div>
+                            <div class="container m-2 p-2">
+                                <div class="d-inline mr-4 p-2 bg-dark text-white"><?= number_format($data_temp[0]); ?></div>
+                                <div class="d-inline mr-4 p-2 bg-dark text-white"><?= number_format($data_temp[1]); ?></div>
+                                <div class="d-inline mr-4 p-2 bg-dark text-white"><?= number_format($data_temp[2]); ?></div>
+                                <div class="d-inline mr-4 p-2 bg-dark text-white"><?= number_format($data_temp[3]); ?></div>
+                            </div>
+                            <div class="container m-2 p-2">
+                                <div class="d-inline p-2 bg-primary text-white">MIN</div>
+                                <div class="d-inline p-2 bg-primary text-white">MAX</div>
+                                <div class="d-inline p-2 bg-primary text-white">MEDIAN</div>
+                                <div class="d-inline p-2 bg-primary text-white">AVERAGE</div>
+                            </div>
+                        </div>
+                        <div class="col-g">
+                            <div class="container-white">
+                                <h2>Humidity</h2>
+                                <canvas id="<?= $id_hum; ?>" width="400" height="400"></canvas>
+                            </div>
+
+                            <div class="container m-2 p-2">
+                                <div class="d-inline mr-4 p-2 bg-dark text-white"><?= number_format($data_hum[0]); ?></div>
+                                <div class="d-inline mr-4 p-2 bg-dark text-white"><?= number_format($data_hum[1]); ?></div>
+                                <div class="d-inline mr-4 p-2 bg-dark text-white"><?= number_format($data_hum[2]); ?></div>
+                                <div class="d-inline mr-4 p-2 bg-dark text-white"><?= number_format($data_hum[3]); ?></div>
+                            </div>
+                            <div class="container m-2 p-2">
+                                <div class="d-inline p-2 bg-primary text-white">MIN</div>
+                                <div class="d-inline p-2 bg-primary text-white">MAX</div>
+                                <div class="d-inline p-2 bg-primary text-white">MEDIAN</div>
+                                <div class="d-inline p-2 bg-primary text-white">AVERAGE</div>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <h3>Latest Value</h3>
-        <div class="container">
-            <div class="d-inline p-2 bg-primary text-white">MIN</div>
-            <div class="d-inline p-2 bg-dark text-white">MAX</div>
-            <div class="d-inline p-2 bg-primary text-white">MEDIAN</div>
-            <div class="d-inline p-2 bg-dark text-white">AVERAGE</div>
+            <?php } ?>
         </div>
     </div>
 </div>
@@ -48,9 +89,150 @@
 
 <?= $this->section('script'); ?>
 <script src="<?= base_url('chartjs/Chart.bundle.min.js') ?>"></script>
+<script src="<?= base_url('chartjs/Chart.bundle.min.js') ?>"></script>
+
 <script>
-    var temperatur = document.getElementById('temperatur');
-    var data_temperatur = [];
-    var label_temperatur = [];
+    // GET CANVAS ID
+    var temperatur1 = document.getElementById('temperatur1');
+    var humidity1 = document.getElementById('humidity1');
+
+    var temperatur2 = document.getElementById('temperatur2');
+    var humidity2 = document.getElementById('humidity2');
+
+    var temperatur3 = document.getElementById('temperatur3');
+    var humidity3 = document.getElementById('humidity3');
+
+    // PARSE DATA
+    var data_temperatur1 = [];
+    var data_humidity1 = [];
+    var label_waktu1 = [];
+    <?php foreach ($data_room1 as $value) : ?>
+        data_temperatur1.push(<?= $value['temperature'] ?>)
+        data_humidity1.push(<?= $value['humidity']; ?>)
+        label_waktu1.push(<?= $value['timeStamp']; ?>)
+    <?php endforeach ?>
+
+    var data_temperatur2 = [];
+    var data_humidity2 = [];
+    var label_waktu2 = [];
+    <?php foreach ($data_room2 as $value) : ?>
+        data_temperatur2.push(<?= $value['temperature'] ?>)
+        data_humidity2.push(<?= $value['humidity']; ?>)
+        label_waktu2.push(<?= $value['timeStamp']; ?>)
+    <?php endforeach ?>
+
+    var data_temperatur3 = [];
+    var data_humidity3 = [];
+    var label_waktu3 = [];
+    <?php foreach ($data_room3 as $value) : ?>
+        data_temperatur3.push(<?= $value['temperature'] ?>)
+        data_humidity3.push(<?= $value['humidity']; ?>)
+        label_waktu3.push(<?= $value['timeStamp']; ?>)
+    <?php endforeach ?>
+
+    // DATA
+    // ROOM1
+    var temp_room1 = {
+        datasets: [{
+            data: data_temperatur1,
+            backgroundColor: [
+                'rgba(255,99,132,0,8)',
+                'rgba(54,162,235,0,8)',
+                'rgba(255,99,132,0,8)',
+            ],
+        }],
+        labels: label_waktu1,
+    }
+
+    var hum_room1 = {
+        datasets: [{
+            data: data_humidity1,
+            backgroundColor: [
+                'rgba(255,99,132,0,8)',
+                'rgba(54,162,235,0,8)',
+                'rgba(255,99,132,0,8)',
+            ],
+        }],
+        labels: label_waktu1,
+    }
+    //  ROOM2
+    var temp_room2 = {
+        datasets: [{
+            data: data_temperatur2,
+            backgroundColor: [
+                'rgba(255,99,132,0,8)',
+                'rgba(54,162,235,0,8)',
+                'rgba(255,99,132,0,8)',
+            ],
+        }],
+        labels: label_waktu2,
+    }
+
+    var hum_room2 = {
+        datasets: [{
+            data: data_humidity2,
+            backgroundColor: [
+                'rgba(255,99,132,0,8)',
+                'rgba(54,162,235,0,8)',
+                'rgba(255,99,132,0,8)',
+            ],
+        }],
+        labels: label_waktu2,
+    }
+    // ROOM3
+    var temp_room3 = {
+        datasets: [{
+            data: data_temperatur3,
+            backgroundColor: [
+                'rgba(255,99,132,0,8)',
+                'rgba(54,162,235,0,8)',
+                'rgba(255,99,132,0,8)',
+            ],
+        }],
+        labels: label_waktu3,
+    }
+
+    var hum_room3 = {
+        datasets: [{
+            data: data_humidity3,
+            backgroundColor: [
+                'rgba(255,99,132,0,8)',
+                'rgba(54,162,235,0,8)',
+                'rgba(255,99,132,0,8)',
+            ],
+        }],
+        labels: label_waktu3,
+    }
+
+    // CHART
+    var chart_temp_room1 = new Chart(temperatur1, {
+        type: 'line',
+        data: temp_room1,
+    });
+
+    var chart_hum_room1 = new Chart(humidity1, {
+        type: 'line',
+        data: hum_room1
+    });
+
+    var chart_temp_room2 = new Chart(temperatur2, {
+        type: 'line',
+        data: temp_room1
+    });
+
+    var chart_hum_room2 = new Chart(humidity2, {
+        type: 'line',
+        data: hum_room1
+    });
+
+    var chart_temp_room3 = new Chart(temperatur3, {
+        type: 'line',
+        data: temp_room1
+    });
+
+    var chart_hum_room3 = new Chart(humidity3, {
+        type: 'line',
+        data: hum_room1
+    });
 </script>
 <?= $this->endSection(); ?>
